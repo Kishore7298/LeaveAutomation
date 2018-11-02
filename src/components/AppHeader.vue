eea03141b18f4b671fe4cb38c53c90b30e7fc2bb
kishore <4nm15is025@nmamit.in>
2018-11-02 23:05:57 +0530

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id='all'>
+   <nav class="navbar bg-dark navbar-dark navbar-expand-sm">
+       <div class="container">
+           <a href="#" class="navbar-brand">Leave Automation</a>
+           <div class="navbar-nav">
+               <a href="#" class="nav-item nav-link active">Login</a>
+           </div>
+       </div>
+   </nav>
+  </div>
+</template>
+<script>
+export default {
+    name:"AppHeader",
+}
+</script>
+<style scoped>
+#all{
+    font-family: 'Poppins', sans-serif;
+}
+</style>
+
+
---
+++ src/components/Form.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+            <h3> Login to continue...</h3>
+    </div>
+</template>
+<script>
+export default {
+    name:"Form"
+};
+</script>
+<style scoped>
+.container{
+    font-family: 'Patrick Hand', cursive;
+}
+</style>
