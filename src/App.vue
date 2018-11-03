27bba28df41198c5900d65a53408ab024bf198cf
kishore <4nm15is025@nmamit.in>
2018-11-03 12:51:34 +0530

    Student login added

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,12 @@
 </template>
 <script>
 import AppHeader from "./components/AppHeader";
+import StudentLogin from "./components/StudentLogin";
 export default {
     name:"App",
     components:{
         AppHeader,
+        StudentLogin
         }
 }
 </script>
---
+++ src/components/StudentLogin.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <form class="form-horizontal">
+            <div class="form-group ">
+                <label class="col-sm-2" for="email">Email:</label>
+                <div class="col-sm-10">
+                    <input type="email" class="form-control" id="email" style="width:50%;" placeholder="Enter email" name="email">
+                 </div>
+            </div>
+             <div class="form-group">
+               <label class="col-sm-2" for="pwd">Password:</label>
+               <div class="col-sm-10">
+                    <input type="password" class="form-control" id="email" style="width:50%;" placeholder="Enter password" name="password">
+                </div>
+             </div>
+             <div class="form-group">        
+                <div class="col-sm-offset-2 col-sm-10">
+                <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</template>
+<script>
+export default {
+    name:"StudentLogin"
+}
+</script>
+<style scoped>
+.container{
+    margin-top: 20px;
+};
+</style>
+
